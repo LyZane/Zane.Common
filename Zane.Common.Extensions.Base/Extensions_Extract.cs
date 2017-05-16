36668ea2b28fdc732bba42ba626163d8f5ce1a85
Zane <lyzane.it@gmail.com>
2017-05-16 13:50:24 +0800

    Add "Extensions_TypeConvert" and "Extensions_Extract".

---
+++ Zane.Common.Extensions.Base/Extensions_Extract.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Zane.Common.Extensions.Base
+{
+    public static class Extensions_Extract
+    {
+        /// <summary>
+        /// 从字符串中抽取出价格,如果没抽到，返回null。
+        /// </summary>
+        /// <param name="priceStr"></param>
+        /// <returns></returns>
+        public static double? ExtractPrice(this string str)
+        {
+            var result = str.ExtractNumber();
+            if (result.Length > 0)
+            {
+                return Math.Abs(result[0]);
+            }
+            return null;
+        }
+        /// <summary>
+        /// 从字符串中抽取出double,如果没抽到，返回null。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static double? ExtractDouble(this string str)
+        {
+            var result = str.ExtractNumber();
+            if (result.Length > 0)
+            {
+                return result[0];
+            }
+            return null;
+        }
+
+        public static double[] ExtractNumber(this string str)
+        {
+            List<double> result = new List<double>();
+            string pattern = @"(?<num>-?((\d{1,3},)?(\d{3},)+\d{3}|\d+)(\.\d+)?) *(?<unit>[k千万]?)";
+            foreach (Match item in Regex.Matches(str.ToLower(),pattern))
+            {
+                string temp = item.Groups["num"].Value;
+                temp = Extensions_String.ToDBC(temp);
+                double num = temp.ToDouble();
+                switch (item.Groups["unit"].Value)
+                {
+                    case "k":
+                    case "千":
+                        num *= 1000;
+                        break;
+                    case "万":
+                        num *= 10000;
+                        break;
+                    default:
+                        break;
+                }
+                result.Add(num);
+            }
+            return result.ToArray();
+        }
+        /// <summary>
+        /// 从字符串中抽取出整数,如果没抽到，返回null。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static int? ExtractInt(this string str)
+        {
+            var result = str.ExtractNumber();
+            if (result.Length > 0)
+            {
+                return Convert.ToInt32(result[0]);
+            }
+            return null;
+        }
+        /// <summary>
+        /// 从字符串中抽取出无符号整数,如果没抽到，返回null。
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static uint? ExtractUInt(this string str)
+        {
+            var result = str.ExtractNumber();
+            if (result.Length > 0)
+            {
+                return Convert.ToUInt32(result[0]);
+            }
+            return null;
+        }
+    }
+}
---
+++ Zane.Common.Extensions.Base/Extensions_TypeConvert.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zane.Common.Extensions.Base
+{
+    public static class Extensions_TypeConvert
+    {
+        public static long ToInt64(this object obj)
+        {
+            return Convert.ToInt64(obj);
+        }
+
+        public static int ToInt32(this object obj)
+        {
+            return Convert.ToInt32(obj);
+        }
+
+        public static double ToDouble(this object obj)
+        {
+            return Convert.ToDouble(obj);
+        }
+
+        public static char ToChar(this object obj)
+        {
+            return Convert.ToChar(obj);
+        }
+        internal static bool IsNullableType(this Type type)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>);
+        }
+        public static object ChangeType(this object obj, Type t)
+        {
+            var value = obj;
+            if (value is DBNull)
+            {
+                value = null;
+            }
+            if (t.IsNullableType() && value ==null)
+            {
+                return null;
+            }
+            //int? ==> int
+            var temp =  Nullable.GetUnderlyingType(t);
+            t = temp == null ? t : temp;
+
+            if (t == typeof(DateTime))//DateTime务必在ValueType前面
+            {
+                return Convert.ToDateTime(value);
+            }
+            else if (t.IsEnum)//enum务必在ValueType前面
+            {
+                if (value == null)
+                {
+                    value = 0;
+                }
+                return Enum.Parse(t, value.ToString());
+            }
+            else if (t.IsValueType)
+            {
+                if (value == null)
+                {
+                    value = 0;
+                }
+                return Convert.ChangeType(value, t);
+            }
+            else if (t == typeof(string))
+            {
+                if (value == null)
+                {
+                    return null;
+                }
+                return value.ToString();
+            }
+            else if (t == typeof(Uri))
+            {
+                if (value==null|| value.ToString()=="")
+                {
+                    return null;
+                }
+                return new Uri(value.ToString());
+            }
+            return null;
+        }
+    }
+}
